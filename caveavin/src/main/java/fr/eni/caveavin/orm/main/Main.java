56a3e052ff4be0ee12d1aa02bfe8e14cae921862
Marc <harnist.marc@gmail.com>
2019-08-28 09:22:46 +0200

    add Spring library
    Go to Eclipse/Window/preferences/gradle and check the good version of
    Gradle

---
+++ caveavin/src/main/java/fr/eni/caveavin/orm/main/Main.java
@@ -0,0 +1,15 @@
+package fr.eni.caveavin.orm.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+
+	}
+
+}
